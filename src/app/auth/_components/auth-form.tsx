35b2fac5d83d4309f34555b0cf1544580882646f
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2024-02-29 14:53:45 -0300

    micro-saas-app functional

---
+++ src/app/auth/_components/auth-form.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { CardTitle, CardDescription, CardHeader, CardContent, Card } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { useForm } from "react-hook-form"
+
+
+export default function AuthForm(){
+    const form = useForm()
+
+    const handleSubmit = form.handleSubmit((data) => {
+        console.log(data)
+    })
+
+
+    return(
+        <Card onSubmit={handleSubmit} className="mx-auto max-w-sm">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl font-bold">Login</CardTitle>
+        <CardDescription>Enter your email below to login to your account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" placeholder="m@example.com" required type="email" {...form.register('email')} />
+          </div>
+          <div className="space-y-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link className="ml-auto inline-block text-sm underline" href="#">
+                Forgot your password?
+              </Link>
+            </div>
+            <Input id="password" required type="password" />
+          </div>
+          <Button className="w-full" type="submit">
+            Login
+          </Button>
+          <Button className="w-full" variant="outline">
+            Login with Google
+          </Button>
+        </form>
+        <div className="mt-4 text-center text-sm">
+          Dont have an account?
+          <Link className="underline" href="#">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+    )
+}
+
+
+
+
+
+
+
